b01001f46d92d18af089c35dff4751cf7270103d
Anastasiia Pozdniakova <44477926+AnastasiiaPozdniakova@users.noreply.github.com>
2021-12-13 14:15:36 +0200

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -6,3 +6,6 @@ VALUES ('Bogdan', 'Khmelnitskyi', 3806524763)
 
 INSERT INTO driver(user_info_id,license_serie,license_number)
 VALUES (1, 'ААА', 253698);
+
+INSERT INTO checkpoint (lat, lon, street_name, house_number, locality_name)
+VALUES (48.861636, 38.467134, 'kvartal 40 rokiv Peremoghi', 12, 'Lysychansk');
