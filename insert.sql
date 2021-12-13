66c21ace570b0eae9870a7d2c7efb15224b4ee73
Anastasiia Pozdniakova <44477926+AnastasiiaPozdniakova@users.noreply.github.com>
2021-12-13 13:59:25 +0200

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO user_info(name, surname, telephone) 
+VALUES ('Bogdan', 'Khmelnitskyi', 3806524763)
+
+INSERT INTO user_info(name, surname, telephone) 
+VALUES ('Bogdan', 'Khmelnitskyi', 3806524763)
+
+INSERT INTO driver(user_info_id,license_serie,license_number)
+VALUES (1, 'ААА', 253698);
