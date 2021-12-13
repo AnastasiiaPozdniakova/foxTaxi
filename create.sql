1cae0ae975beaf959e54024efcf0f9625fe58778
Anastasiia Pozdniakova <44477926+AnastasiiaPozdniakova@users.noreply.github.com>
2021-12-13 13:39:26 +0200

    Create create.sql

---
+++ create.sql
@@ -0,0 +1,81 @@
+CREATE DATABASE FoxTaxi;
+
+use FoxTaxi;
+
+
+CREATE TABLE IF NOT EXISTS user_info(
+id SERIAL  PRIMARY KEY,
+Name varchar (50),
+Surname varchar (50),
+Telephone NUMERIC,
+google_id varchar (50),
+facebook_id varchar (50),
+email varchar (50)
+);
+
+CREATE TABLE IF NOT EXISTS vehicle_type(
+id SERIAL PRIMARY KEY,
+Name varchar(50)
+);
+
+CREATE TABLE IF NOT EXISTS driver(
+user_info_id int UNIQUE NOT NULL REFERENCES user_info(id) ,
+license_serie varchar(4) ,
+license_number int NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS vehicle(
+    gov_number varchar(50) PRIMARY KEY,
+	vehicle_type_id int NOT NULL REFERENCES vehicle_type(id),
+	driver_user_info_id int NOT NULL REFERENCES driver(user_info_id)
+);
+
+
+
+CREATE TABLE IF NOT  EXISTS payment_type(
+id SERIAL PRIMARY KEY,
+	name varchar(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS payment_info(
+id SERIAL PRIMARY KEY,
+	amount numeric,
+	payment_type_id int NOT NULL REFERENCES payment_type(id)	
+);
+
+CREATE TABLE IF NOT EXISTS checkpoint (
+  id SERIAL PRIMARY KEY,
+  lat numeric,
+  lon numeric,
+  street_name varchar(50),
+  house_number varchar(50),
+  locality_name varchar(50)
+);
+
+CREATE TABLE IF NOT EXISTS payment (
+   id SERIAL PRIMARY KEY,
+	totla_amount numeric,
+	payment_info_card_id int NULL REFERENCES payment_info(id),
+	payment_info_cash_id int NULL REFERENCES payment_info(id),
+	payment_info_bonus_id int NULL REFERENCES payment_info(id)
+);
+  
+CREATE TABLE IF NOT EXISTS ride(
+    id SERIAL PRIMARY KEY,
+	 beginning_checkpoint_id int NOT NULL REFERENCES checkpoint(id),
+	 destinantion_checkpoint_id int NOT NULL REFERENCES checkpoint(id), 
+	 payment_id int NULL REFERENCES payment(id),
+	 driver_user_info_id int NOT NULL REFERENCES driver(user_info_id)
+);
+
+CREATE TABLE ride_details(
+    ride_detail_id SERIAL PRIMARY KEY,
+	checkpoint_id_1 int NULL REFERENCES checkpoint,
+	checkpoint_id_2 int NULL REFERENCES checkpoint,
+	checkpoint_id_3 int NULL REFERENCES checkpoint,
+	ride_id int NULL REFERENCES ride(id)
+);
+
+
+
+
