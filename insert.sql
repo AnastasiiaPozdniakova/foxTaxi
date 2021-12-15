19c7c879730d38a8d9c624f65d79df776ed9dc1b
Anastasiia Pozdniakova <44477926+AnastasiiaPozdniakova@users.noreply.github.com>
2021-12-15 13:32:13 +0200

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -8,4 +8,16 @@ INSERT INTO driver(user_info_id,license_serie,license_number)
 VALUES (1, 'ААА', 253698);
 
 INSERT INTO checkpoint (lat, lon, street_name, house_number, locality_name)
+VALUES (48.885561	,38.479120,	"Chervona"	,12	,"Lysychansk");
+
+INSERT INTO checkpoint (lat, lon, street_name, house_number, locality_name)
+VALUES (48.914019	,38.418236,	"prospekt Peremohy",	86,	"Lysychansk");
+
+INSERT INTO checkpoint (lat, lon, street_name, house_number, locality_name)
 VALUES (48.861636, 38.467134, 'kvartal 40 rokiv Peremoghi', 12, 'Lysychansk');
+
+INSERT INTO ride (beginning_checkpoint_id, destination_checkpoint_id, payment_id, driver_user_info_id)
+VALUES (1, 2, null, 1 );
+
+INSERT INTO ride_details (checkpoint_id_1, checkpoint_id_2, checkpoint_id_3)
+VALUES (3, null, null);
